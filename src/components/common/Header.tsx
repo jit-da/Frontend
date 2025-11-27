d46bdd23a8d3c9b4412a15008459fae68306333c
Hayoung Kang <rkdgkdudsam@naver.com>
2025-11-28 05:57:10 +0900

    feat: Header 컴포넌트 퍼블리싱

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,43 @@
+import { ChevronLeft, Search, Handbag } from "lucide-react";
+
+export default function Header({
+    children,
+    back,
+    logo,
+    search,
+    cart,
+}: {
+    children?: React.ReactNode;
+    back?: boolean;
+    logo?: boolean;
+    search?: boolean;
+    cart?: boolean;
+}) {
+    return (
+        <>
+            <header className="z-2 fixed w-full max-w-md h-15.5 flex justify-center items-center bg-white/85 backdrop-blur-[2px]">
+                <div className="absolute left-5">
+                    {back && <ChevronLeft size={28} strokeWidth={1.5} />}
+                    {logo && (
+                        <span className="gmarket-sans text-[32px] text-main500">
+                            ㅈㄷ
+                        </span>
+                    )}
+                </div>
+                <h1>{children}</h1>
+                <div className="absolute right-3 flex">
+                    {search && (
+                        <button className="w-10 h-10 flex justify-center items-center hover:bg-gray100 rounded-lg">
+                            <Search strokeWidth={1.5} />
+                        </button>
+                    )}
+                    {cart && (
+                        <button className="w-10 h-10 flex justify-center items-center hover:bg-gray100 rounded-lg">
+                            <Handbag strokeWidth={1.5} />
+                        </button>
+                    )}
+                </div>
+            </header>
+        </>
+    );
+}
