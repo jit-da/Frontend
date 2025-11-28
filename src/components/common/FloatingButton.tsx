0b33485e1530f92e46780b47f0ad4345fc1797ef
Hayoung Kang <rkdgkdudsam@naver.com>
2025-11-28 17:34:48 +0900

    fix: FloatingButton 컴포넌트 오타 수정

--- src/components/common/FloatingButton.tsx
+++ src/components/common/FloatingButton.tsx
@@ -14,7 +14,7 @@ export default function FloatingButton({
             <div className="fixed w-full bottom-36">
                 <div className="relative z-2 w-full max-w-md">
                     <button
-                        className="floating-btn duration-100 flex flex-col bg-gray1000 hover:bg-gray900 ease-out action:bg-gray900 text-white"
+                        className="floating-btn duration-100 flex flex-col bg-gray1000 hover:bg-gray900 ease-out active:bg-gray900 text-white"
                         onClick={onClick}
                     >
                         {icon}
