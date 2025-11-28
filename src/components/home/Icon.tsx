8ff49100b1691c780822a3fb5f3081dc861526b0
Hayoung Kang <rkdgkdudsam@naver.com>
2025-11-28 18:46:52 +0900

    feat: img 태그에 alt 추가

--- src/components/home/Icon.tsx
+++ src/components/home/Icon.tsx
@@ -1,16 +1,14 @@
-import type React from "react";
-
 export default function Icon({
     children,
     icon,
 }: {
-    children: React.ReactNode;
+    children: string;
     icon: string;
 }) {
     return (
         <>
             <button className="full flex flex-col gap-1 items-center hover:bg-gray100 active:bg-gray100 duration-100 ease-out rounded-xl py-2">
-                <img src={icon} className="w-13 h-13" />
+                <img src={icon} alt={children} className="w-13 h-13" />
                 <span className="text-xs">{children}</span>
             </button>
         </>
