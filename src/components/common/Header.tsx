c73cf5d4add7f3f7be4d104503dc6bddf03590ed
Hayoung Kang <rkdgkdudsam@naver.com>
2025-11-28 19:14:14 +0900

    refactor: 스타일 클래스 분리

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -15,7 +15,7 @@ export default function Header({
 }) {
     return (
         <>
-            <header className="z-2 fixed w-full max-w-md h-15.5 flex justify-center items-center bg-white/85 backdrop-blur-[2px]">
+            <header className="z-2 fixed w-full max-w-md h-15.5 center bg-white/85 backdrop-blur-[2px]">
                 <div className="absolute left-5">
                     {back && <ChevronLeft size={28} strokeWidth={1.5} />}
                     {logo && (
@@ -27,12 +27,12 @@ export default function Header({
                 <h1>{children}</h1>
                 <div className="absolute right-3 flex">
                     {search && (
-                        <button className="w-10 h-10 flex justify-center items-center hover:bg-gray100 active:bg-gray100 duration-100 ease-out rounded-lg">
+                        <button className="header-btn">
                             <Search strokeWidth={1.5} />
                         </button>
                     )}
                     {cart && (
-                        <button className="w-10 h-10 flex justify-center items-center hover:bg-gray100 active:bg-gray100 duration-100 ease-out rounded-lg">
+                        <button className="header-btn">
                             <Handbag strokeWidth={1.5} />
                         </button>
                     )}
