756c212d5ad779d90f9cf7d56ec96e30b5793cc6
Hayoung Kang <rkdgkdudsam@naver.com>
2025-12-03 21:15:12 +0900

    feat: Tab 컴포넌트 퍼블리싱

---
+++ src/components/common/Tab.tsx
@@ -0,0 +1,37 @@
+import { RECEIPT_LIST } from "@/constants/receipt";
+
+export default function Tab({
+    select,
+    setSelect,
+}: {
+    select: string;
+    setSelect: (s: string) => void;
+}) {
+    return (
+        <>
+            <nav className="relative top-15.5 w-full max-w-md h-10 bg-white px-5 grid grid-cols-2 pretendard-medium border-b border-gray200">
+                {RECEIPT_LIST.map((i) => (
+                    <button
+                        key={i.id}
+                        className={`flex justify-center items-center duration-200 ${
+                            select !== i.name && "text-gray500"
+                        }`}
+                        onClick={() => setSelect(i.name)}
+                    >
+                        {i.name}
+                    </button>
+                ))}
+                <div
+                    className="absolute bottom-0 h-0.5 bg-gray1000 duration-200"
+                    style={{
+                        width: "calc((100% - 40px) / 2)",
+                        left:
+                            select === RECEIPT_LIST[0].name
+                                ? "20px"
+                                : "calc((100% - 40px) / 2 + 20px)",
+                    }}
+                ></div>
+            </nav>
+        </>
+    );
+}
