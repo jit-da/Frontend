48f2d474b169b09240164fbc063c721a2da2b264
Hayoung Kang <rkdgkdudsam@naver.com>
2025-12-09 00:14:47 +0900

    feat: Card 컴포넌트 퍼블리싱

---
+++ src/components/common/Card.tsx
@@ -0,0 +1,50 @@
+import { Handbag, Heart } from "lucide-react";
+
+export default function Card({
+    title,
+    sub,
+    price,
+    sale = 0,
+    like = false,
+}: {
+    title: string;
+    sub: string;
+    price: number;
+    sale: number;
+    like?: boolean;
+}) {
+    return (
+        <>
+            <button className="w-full flex flex-row gap-3 items-center text-start">
+                <div className="w-30 h-30 min-w-30 border border-gray200 rounded-xl mb-1" />
+                <div className="w-full">
+                    <h4>{title}</h4>
+                    <h5 className="text-sm text-gray600 -mt-1">{sub}</h5>
+                    <p className="pretendard-semibold">
+                        <span className="text-red-500">
+                            {sale !== 0 && sale.toString() + "% "}
+                        </span>
+                        {(price * (1 - sale / 100)).toLocaleString("ko-KR")}원
+                        {sale !== 0 && (
+                            <del className="ml-1 text-gray500 pretendard-regular text-xs">
+                                {price}원
+                            </del>
+                        )}
+                    </p>
+                    <div className="w-full mt-1 flex gap-3 justify-end">
+                        <Handbag strokeWidth={1.5} />
+                        {like ? (
+                            <Heart
+                                strokeWidth={1.5}
+                                fill="#fb2c36"
+                                color="#fb2c36"
+                            />
+                        ) : (
+                            <Heart strokeWidth={1.5} />
+                        )}
+                    </div>
+                </div>
+            </button>
+        </>
+    );
+}
