f18bee074913b8186e7050d4184a142a4f4dad54
Hayoung Kang <rkdgkdudsam@naver.com>
2025-12-09 03:07:09 +0900

    refactor: Mypage/Grade 컴포넌트 반복연산 최적화

--- src/components/mypage/Grade.tsx
+++ src/components/mypage/Grade.tsx
@@ -1,10 +1,12 @@
 import { GRADE_LIST } from "@/constants/mypage";
 
 export default function Grade({ grade }: { grade: string }) {
+    const GRADE = GRADE_LIST.find((i) => i.grade === grade)!;
+
     return (
         <button
             className={`relative mx-5 px-5 py-8 flex justify-between items-center rounded-2xl
-                ${GRADE_LIST.find((i) => i.grade === grade)?.color}`}
+                ${GRADE.color}`}
         >
             <p className="absolute top-4 text-xs pretendard-light text-white">
                 회원님의 등급은
@@ -18,9 +20,7 @@ export default function Grade({ grade }: { grade: string }) {
             >
                 {grade}
             </h3>
-            <p className="text-sm text-white">
-                {GRADE_LIST.find((i) => i.grade === grade)?.benefit}
-            </p>
+            <p className="text-sm text-white">{GRADE.benefit}</p>
             <div className="absolute bottom-4 w-[calc(100%-40px)] h-1 rounded-full bg-white/50" />
             <div className="absolute bottom-4 w-[calc((100%-40px)/100*65)] h-1 rounded-full bg-white" />
         </button>
