3acf1f2d9458cc1d9df1c885490851c3b3a2fedf
Hayoung Kang <rkdgkdudsam@naver.com>
2025-12-09 00:28:42 +0900

    feat: Like 페이지 퍼블리싱

---
+++ src/test/like.ts
@@ -0,0 +1,14 @@
+export const LIKE_LIST = [
+    {
+        name: "아이소 핑크",
+        sub: "10t",
+        price: 2000,
+        sale: 0,
+    },
+    {
+        name: "파이프 원형 롱소켓",
+        sub: "40mm",
+        price: 2800,
+        sale: 10,
+    },
+];
