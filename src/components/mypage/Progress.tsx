d60cd6a35fcaf6aa3add3eb9aa7cb1b86492a887
Hayoung Kang <rkdgkdudsam@naver.com>
2025-12-09 02:12:02 +0900

    feat: Mypage/Progress 컴포넌트 퍼블리싱

---
+++ src/components/mypage/Progress.tsx
@@ -0,0 +1,46 @@
+import { ChevronRight } from "lucide-react";
+
+export default function Progress() {
+    return (
+        <div className="px-5 py-3">
+            <div className="flex justify-between">
+                주문/배송 조회
+                <button className="text-sm text-gray600">전체보기</button>
+            </div>
+            <ul className="w-full flex justify-between items-center text-gray500 mt-2 px-2">
+                <li className="flex flex-col items-center">
+                    <p className="pretendard-semibold text-xl">0</p>
+                    <span className="text-sm">주문접수</span>
+                </li>
+                <li>
+                    <ChevronRight strokeWidth={1.5} />
+                </li>
+                <li className="flex flex-col items-center">
+                    <p className="pretendard-semibold text-xl">0</p>
+                    <span className="text-sm">결제완료</span>
+                </li>
+                <li>
+                    <ChevronRight strokeWidth={1.5} />
+                </li>
+                <li className="flex flex-col items-center">
+                    <p className="pretendard-semibold text-xl">0</p>
+                    <span className="text-sm">상품준비중</span>
+                </li>
+                <li>
+                    <ChevronRight strokeWidth={1.5} />
+                </li>
+                <li className="flex flex-col items-center">
+                    <p className="pretendard-semibold text-xl">0</p>
+                    <span className="text-sm">배송·픽업중</span>
+                </li>
+                <li>
+                    <ChevronRight strokeWidth={1.5} />
+                </li>
+                <li className="flex flex-col items-center text-gray1000">
+                    <p className="pretendard-semibold text-xl">3</p>
+                    <span className="text-sm">인수완료</span>
+                </li>
+            </ul>
+        </div>
+    );
+}
