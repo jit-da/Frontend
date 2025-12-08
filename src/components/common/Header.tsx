8f80c9e1c6cad83e057b0bbead4922d5b3a0e108
Hayoung Kang <rkdgkdudsam@naver.com>
2025-12-09 05:52:19 +0900

    feat: Header 컴포넌트 뒤로가기 기능 연결

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,4 +1,5 @@
 import { ChevronLeft, Search, Handbag } from "lucide-react";
+import { useNavigate } from "react-router";
 
 export default function Header({
     children,
@@ -13,11 +14,20 @@ export default function Header({
     search?: boolean;
     cart?: boolean;
 }) {
+    const navigate = useNavigate();
+
     return (
         <>
             <header className="z-2 fixed w-full max-w-md h-15.5 center bg-white/85 backdrop-blur-[2px]">
                 <div className="absolute left-5">
-                    {back && <ChevronLeft size={28} strokeWidth={1.5} />}
+                    {back && (
+                        <button
+                            className="header-btn"
+                            onClick={() => navigate(-1)}
+                        >
+                            <ChevronLeft size={28} strokeWidth={1.5} />
+                        </button>
+                    )}
                     {logo && (
                         <span className="gmarket-sans text-[32px] text-main500">
                             ㅈㄷ
