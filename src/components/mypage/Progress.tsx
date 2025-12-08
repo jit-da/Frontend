758aac5560db5284efed3e76ea1d3debcafae6ec
Hayoung Kang <rkdgkdudsam@naver.com>
2025-12-09 03:36:59 +0900

    fix: 모바일화면에서 Mypage/Progress 깨지는 오류 수정

--- src/components/mypage/Progress.tsx
+++ src/components/mypage/Progress.tsx
@@ -7,38 +7,38 @@ export default function Progress() {
                 주문/배송 조회
                 <button className="text-sm text-gray600">전체보기</button>
             </div>
-            <ul className="w-full flex justify-between items-center text-gray500 mt-2 px-2">
+            <ul className="w-full flex justify-between items-center text-gray500 my-2 px-2">
                 <li className="flex flex-col items-center">
                     <p className="pretendard-semibold text-xl">0</p>
-                    <span className="text-sm">주문접수</span>
+                    <span className="text-xs">주문접수</span>
                 </li>
                 <li>
                     <ChevronRight strokeWidth={1.5} />
                 </li>
                 <li className="flex flex-col items-center">
                     <p className="pretendard-semibold text-xl">0</p>
-                    <span className="text-sm">결제완료</span>
+                    <span className="text-xs">결제완료</span>
                 </li>
                 <li>
                     <ChevronRight strokeWidth={1.5} />
                 </li>
                 <li className="flex flex-col items-center">
                     <p className="pretendard-semibold text-xl">0</p>
-                    <span className="text-sm">상품준비중</span>
+                    <span className="text-xs">상품준비중</span>
                 </li>
                 <li>
                     <ChevronRight strokeWidth={1.5} />
                 </li>
                 <li className="flex flex-col items-center">
                     <p className="pretendard-semibold text-xl">0</p>
-                    <span className="text-sm">배송·픽업중</span>
+                    <span className="text-xs">배송·픽업중</span>
                 </li>
                 <li>
                     <ChevronRight strokeWidth={1.5} />
                 </li>
                 <li className="flex flex-col items-center text-gray1000">
                     <p className="pretendard-semibold text-xl">3</p>
-                    <span className="text-sm">인수완료</span>
+                    <span className="text-xs">인수완료</span>
                 </li>
             </ul>
         </div>
